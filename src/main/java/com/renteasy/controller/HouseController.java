382b800b1a43a92ba408a28428e402c7c7928e3a
Sunil H <hsunil@gmail.com>
2024-02-24 19:26:11 +0530

    Added House controller/HouseController.java

---
+++ src/main/java/com/renteasy/controller/HouseController.java
@@ -0,0 +1,9 @@
+package com.renteasy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/house")
+public class HouseController {
+}
