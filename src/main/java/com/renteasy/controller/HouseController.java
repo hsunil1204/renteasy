242670fcb1387b1c4b2bcdbf097e4d0d7e59a0d9
Sunil H <hsunil@gmail.com>
2024-02-24 22:01:49 +0530

    New Controller added

--- src/main/java/com/renteasy/controller/HouseController.java
+++ src/main/java/com/renteasy/controller/HouseController.java
@@ -1,9 +1,4 @@
 package com.renteasy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/house")
 public class HouseController {
 }
